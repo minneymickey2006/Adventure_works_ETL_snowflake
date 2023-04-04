49daa55d58cebf0917f26bacbda80df6b4a69702
Puja Gupta <55056612+minneymickey2006@users.noreply.github.com>
2023-04-04 17:56:31 +0530

    Delete Adventure_Works/macros/models/serving directory

--- Adventure_Works/macros/models/serving/dim_salesorderdetail_scd.sql
+++
@@ -1,9 +0,0 @@
-with snp_salesorderdetail as (
-    select
-        {{ dbt_utils.surrogate_key(['salesorderdetailid', 'dbt_valid_from', 'dbt_valid_to']) }} as snp_orderdetailid_key 
-        , *
-    from {{ ref('snp_salesorderdetail') }}
-) 
-
-select * 
-from snp_salesorderdetail
